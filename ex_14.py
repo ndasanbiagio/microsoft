c0f5814850dd5992e4286b7964980b97788d5d62
ndasanbiagio <ndasanbiagio@gmail.com>
2023-12-17 18:18:54 -0300

    v8

--- ex_14.py
+++ ex_14.py
@@ -5,3 +5,17 @@
 # Por ejemplo si al invocar esta función pasamos la palabra
 # "entretenido"
 # , debería devolver ['d','e','i','n','o','r','t']
+
+def letras_unicas(palabra):
+    
+    mi_set = set()
+    
+    for letra in palabra:
+        mi_set.add(letra)
+    
+    mi_lista = list(mi_set)
+    mi_lista.sort()
+    
+    return mi_lista
+
+print(letras_unicas("entretenido"))
